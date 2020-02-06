f5899fa8c0fd56abe871fa0b2757628082242a4f
studyfree20 <studyfree20@gmail.com>
2020-02-06 03:41:44 -0800

    Divisibility tests

---
+++ Divisibilityby5.py
@@ -0,0 +1,9 @@
+'''
+Divisibility by 5
+emoji included for kids program
+'''
+ #start of code
+number =float(input(" Please enter your number : "))                              #fetching data from user
+if(number % 5 == 0):print("Number is Divisible by 5\U0001f600 \U0001f600") 
+else:print("Number is not Divisible try again \U0001F611")
+#end of code
