2607b3eb770634b79a682ab3ccd87bfa4cc3ce34
studyfree20 <studyfree20@gmail.com>
2020-02-06 03:37:27 -0800

    Divisibility tests

---
+++ Divisibilityby2.py
@@ -0,0 +1,9 @@
+'''
+Create a program to check if a number is divisible by 2
+emoji icluded for kids
+'''
+ #start of code
+number =float(input(" Please enter your number : "))                              #fetching data from user
+if(number % 2 == 0):print("Number is Divisible by 2\U0001f600 \U0001f600") 
+else:print("Number is not Divisible try again \U0001F611")
+#end of code
